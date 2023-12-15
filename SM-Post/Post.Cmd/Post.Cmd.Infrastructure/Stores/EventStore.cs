36f670ec2dd1bb2ab8d619330cf2a1b6107feb5a
Joakin <jalberto2891@gmail.com>
2023-12-15 14:54:41 -0500

    [SMP-12] Add EventStore class.

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -0,0 +1,60 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregates;
+
+namespace Post.Cmd.Infrastructure.Stores
+{
+    public class EventStore : IEventStore
+    {
+        private readonly IEventStoreRepository eventStoreRepository;
+
+        public EventStore(IEventStoreRepository eventStoreRepository)
+        {
+            this.eventStoreRepository = eventStoreRepository;
+        }
+
+        public async Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId)
+        {
+            List<EventModel> eventStream = await this.eventStoreRepository.FindByAggregateId(aggregateId);
+
+            if (eventStream == null || !eventStream.Any())
+            {
+                throw new AggregateNotFoundException("Incorrect post ID provided!");
+            }
+
+            return eventStream.OrderBy(x => x.Version).Select(x => x.EventData).ToList();
+        }
+
+        public async Task SaveEventsAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectedVersion)
+        {
+            List<EventModel> eventStream = await this.eventStoreRepository.FindByAggregateId(aggregateId);
+
+            if (expectedVersion != -1 && eventStream[^1].Version != expectedVersion)
+            {
+                throw new ConcurrencyException();
+            }
+
+            int version = expectedVersion;
+
+            foreach (BaseEvent @event in events)
+            {
+                version++;
+                @event.Version = version;
+                string eventType = @event.GetType().Name;
+                var eventModel = new EventModel
+                {
+                    TimeStamp = DateTime.Now,
+                    AggregateIdentifier = aggregateId,
+                    AggregateType = nameof(PostAggregate),
+                    Version = version,
+                    EventType = eventType,
+                    EventData = @event
+                };
+
+                await this.eventStoreRepository.SaveAsync(eventModel);
+            }
+        }
+    }
+}
