0aa839dfaba58f037a8669f44d1ff1bbdd36bec5
Joakin <jalberto2891@gmail.com>
2023-12-15 13:06:56 -0500

    Implement the mongo repository for the event store model.

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,31 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Post.Cmd.Infrastructure.Config;
+
+namespace Post.Cmd.Infrastructure.Repositories
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly IMongoCollection<EventModel> eventStoreCollection;
+
+        public EventStoreRepository(IOptions<MongoDbConfig> config)
+        {
+            var mongoClient = new MongoClient(config.Value.ConnectionString);
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(config.Value.Database);
+
+            this.eventStoreCollection = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
+        }
+
+        public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
+        {
+            return await this.eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task SaveAsync(EventModel @event)
+        {
+            await this.eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+        }
+    }
+}
